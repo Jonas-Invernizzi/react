eb0c2e532772e2c36f12f45ae74225546c6ac1cf
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-08-05 11:36:19 -0300

    first commit

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,8 @@
+import tungtungsahur from "./assets/triple-t.webp";
+function Paragrafo() {
+  return (<>
+  <p>Parágrafo Explicativo</p>
+  <p><img src={tungtungsahur} width = {100}/></p>
+  </>);
+}
+export default Paragrafo;
