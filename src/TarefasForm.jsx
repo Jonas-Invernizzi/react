d30f3c7c9ee6c4d9349e2927c8b4d3430b8b01d4
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-08-12 11:26:28 -0300

    Adição de Itens

---
+++ src/TarefasForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const TarefasForm = ({ onAddTarefa }) => {
+    const [tarefa, setTarefa] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if(tarefa.trim()){
+            onAddTarefa(tarefa.trim())
+            setTarefa('');
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={tarefa} placeholder="Digiter sua tarefa" onChange={(e) => {setTarefa(e.target.value)}}/>
+            <button type="submit">Adicionar</button>
+        </form>
+    )
+}
+export default TarefasForm
