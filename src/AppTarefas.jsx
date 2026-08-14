e8f9dfdd1cc43233f4dafd5f7a31ab44e2c9a6df
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-08-14 08:59:24 -0300

    Edição e Delete

--- src/AppTarefas.jsx
+++ src/AppTarefas.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 import TarefasLista from "./TarefasLista";
 import TarefasForm from "./TarefasForm";
 
 const AppTarefas = () => {
-    const [tarefas, setTarefas] = useState([])
-    const addTarefa = (texto) => {
-        const novaTarefa = {
-            id: Date.now(),
-            texto: texto,
-        }
-        setTarefas([...tarefas, novaTarefa])
-    }
-    return (
-        <div>
-            <h1>💡Keepão React</h1>
-            <TarefasForm onAddTarefa={addTarefa} />
-            <TarefasLista tarefas={tarefas}/>
-            
-        </div>
-    );
-}
-export default AppTarefas
+  const [tarefas, setTarefas] = useState([]);
+  const addTarefa = (texto) => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: texto,
+    };
+    setTarefas([...tarefas, novaTarefa]);
+  };
+  const editTarefa = (id, novoTexto) => {
+    setTarefas(tarefas.map(tarefa => tarefa.id === id ? {...tarefa, texto:novoTexto} : tarefa))
+  };
+  const deleteTarefa = (id) => {
+    setTarefas(tarefas.filter((tarefa) => tarefa.id !== id));
+  };
+  return (
+    <div>
+      <h1>💡Keepão React</h1>
+      <TarefasForm onAddTarefa={addTarefa}/>
+      <TarefasLista tarefas={tarefas} onEditTarefa={editTarefa} onDeleteTarefa={deleteTarefa} />
+    </div>
+  );
+};
+export default AppTarefas;
